0e77dcef382ec822e03c2fe08b437065201a251b
jhlee <wisdom_13_@naver.com>
2023-05-24 04:00:35 +0900

    Sequelize 설치

--- commands/fun/dice.js
+++ commands/fun/dice.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('D6')
-    .setDescription('Roll the dice 🎲'),
+    .setDescription('Roll the dice! 🎲'),
   async execute(interaction) {
     await interaction.reply(``);
   },
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
+
+const Sequelize = require('sequelize');
+
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
