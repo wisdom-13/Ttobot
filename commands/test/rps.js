e42ac2bbcdd02ab59494cab9ec16c77f79b8c4af
jhlee <wisdom_13_@naver.com>
2023-08-12 17:34:13 +0900

    코드 정리

--- commands/test/rps.js
+++ commands/test/rps.js
@@ -4,7 +4,7 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 module.exports = {
   name: '가위바위보',
 
-  async run(msg, args) {
+  async run(msg) {
     console.log(msg.author)
     const Rock = new ButtonBuilder()
       .setCustomId('Rock')
@@ -24,31 +24,11 @@ module.exports = {
     const row = new ActionRowBuilder()
       .addComponents(Scissors, Rock, Paper);
 
-    const response = await msg.reply({
+    msg.reply({
       content: `🕹️ 버튼을 클릭하세요!`,
       components: [row],
     });
 
-    const collectorFilter = i => i.user.id === interaction.user.id;
-
-    // try {
-    //   const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
-    //   console.log('기다리는중')
-    //   console.log(confirmation)
-
-    //   if (confirmation.customId === 'Rock') {
-    //     await confirmation.update({ content: `주먹을 냈다.`, components: [] });
-
-    //   } else if (confirmation.customId === 'Paper') {
-    //     await confirmation.update({ content: '보를 냈다.', components: [] });
-
-    //   } else if (confirmation.customId === 'Scissors') {
-    //     await confirmation.update({ content: '가위를 냈다.', components: [] });
-    //   }
-
-    // } catch (e) {
-    //   await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
-    // }
   },
 
 };
